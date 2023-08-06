851998e1cad7d79c9df1897f68406cb5c5961a47
HarveyWho <haoxuanhong5@gmail.com>
2023-08-06 15:43:32 +0800

    Updated index.html and gameFunctions.js

--- gameFunctions.js
+++ gameFunctions.js
@@ -1,50 +1,90 @@
-function playerChoose() {
-    let playerSelection = prompt("Please input one of the three following: Rock, Paper, Scissors")
-    return playerSelection;
+// Event Listeners for the buttons
+document.querySelector(".Rock").addEventListener("click", function() { playRound("Rock") });
+document.querySelector(".Paper").addEventListener("click", function() { playRound("Paper") });
+document.querySelector(".Scissors").addEventListener("click", function() { playRound("Scissors") }); // I've changed "Scissor" to "Scissors" here
+
+var playerWin = 0;
+var computerWin = 0;
+
+function displayMessage(message) {
+    document.getElementById('results').textContent = message;
 }
 
-function playRound (playerSelection) {
-    let comp = computerSelect()
-    alert(`Computer selected ${comp}. You selected ${playerSelection}. Let's see!`)
+function updateScoreboard() {
+    document.getElementById('scoreboard').textContent = `Player: ${playerWin} - Computer: ${computerWin}`;
+}
+
+function playRound(playerSelection) {
+    let comp = computerSelect();
+    displayMessage(`Computer selected ${comp}. You selected ${playerSelection}. Let's see!`);
+    
     switch (comp) {
         case "Rock":
-            if(playerSelection=="Scissors")
-                alert("Ooops, scissors beat by rock.")
-            else if (playerSelection=="Paper")
-                alert(`Hooray, you beat rock! This is the ${++playerWin}nd game you win`)
+            if (playerSelection === "Scissors") {
+                computerWin++;
+                displayMessage("Ooops, scissors beat by rock.");
+            }
+            else if (playerSelection === "Paper") {
+                playerWin++;
+                displayMessage("Hooray, you beat rock!");
+            }
             else
-                alert("Oh oh, it's a draw.")
+                displayMessage("Oh oh, it's a draw.");
             break;
         case "Paper":
-            if(playerSelection=="Rock")
-                alert("Ooops, rock beat by paper.")
-            else if (playerSelection=="Scissors")
-                alert(`Hooray, you beat paper! This is the ${++playerWin}nd game you win`)
+            if (playerSelection === "Rock") {
+                computerWin++;
+                displayMessage("Ooops, rock beat by paper.");
+            }
+            else if (playerSelection === "Scissors") {
+                playerWin++;
+                displayMessage("Hooray, you beat paper!");
+            }
             else
-                alert("Oh oh, it's a draw.")
+                displayMessage("Oh oh, it's a draw.");
             break;
         case "Scissors":
-            if(playerSelection=="Paper")
-                alert("Ooops, paper beat by scissors.")
-            else if (playerSelection=="Rock")
-                alert(`Hooray, you beat scissors! This is the ${++playerWin}nd game you win`)
+            if (playerSelection === "Paper") {
+                computerWin++;
+                displayMessage("Ooops, paper beat by scissors.");
+            }
+            else if (playerSelection === "Rock") {
+                playerWin++;
+                displayMessage("Hooray, you beat scissors!");
+            }
             else
-                alert("Oh oh, it's a draw.")
+                displayMessage("Oh oh, it's a draw.");
             break;
         default:
-            alert(`Your input ${playerSelection} is incorrect. Please use Rock, Paper, or Scissors.`)
+            displayMessage(`Your input ${playerSelection} is incorrect. Please use Rock, Paper, or Scissors.`)
             break;
+    }
+
+    updateScoreboard();
+
+    if (playerWin === 5) {
+        displayMessage("Congratulations! You won the game!");
+        playerWin = 0;
+        computerWin = 0;
+        updateScoreboard();
+    }
+    else if (computerWin === 5) {
+        displayMessage("Sorry, the computer won the game.");
+        playerWin = 0;
+        computerWin = 0;
+        updateScoreboard();
     }
 }
 
+
 function computerSelect() {
-    var randomSelection = Math.floor(Math.random()*3)
+    var randomSelection = Math.floor(Math.random() * 3);
     switch (randomSelection) {
         case 0:
-            return "Rock"
+            return "Rock";
         case 1:
-            return "Scissors"
+            return "Scissors";
         case 2:
-            return "Paper"
+            return "Paper";
     }
 }
